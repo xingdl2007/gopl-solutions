f0abfbcd1ea9ee5b12d783a2105eb51212d8172e
XingDL <xingdl2007@gmail.com>
2017-11-13 09:46:51 +0800

    8.5 done

---
+++ ch8/8.5/main.go
@@ -0,0 +1,81 @@
+// Copyright © 2017 xingdl2007@gmail.com
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 61.
+//!+
+
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"image"
+	"image/color"
+	"math/cmplx"
+	"sync"
+	"time"
+	"fmt"
+)
+
+// N represent the number of goroutines
+const N = 128
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	start := time.Now()
+	// partition jobs
+	size := height / N
+	var wg sync.WaitGroup
+	var done = make(chan struct{})
+
+	for i := 0; i < N; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			for py := size * i; py < size*(i+1); py++ {
+				y := float64(py)/height*(ymax-ymin) + ymin
+				for px := 0; px < width; px++ {
+					x := float64(px)/width*(xmax-xmin) + xmin
+					z := complex(x, y)
+
+					// Image point (px, py) represents complex value z.
+					img.Set(px, py, mandelbrot(z))
+				}
+			}
+			done <- struct{}{}
+		}(i)
+	}
+
+	// closer
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	// drain done channel
+	for range done {
+	}
+
+	// skip output
+	//png.Encode(os.Stdout, img) // NOTE: ignoring errors
+	fmt.Printf("elapsed %fms\n", time.Since(start).Seconds()*1000)
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
